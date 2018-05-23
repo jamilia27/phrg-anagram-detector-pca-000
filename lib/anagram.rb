33400f8fb57a14f82e49ea4b3946bcfa3ca46475
Katie Weber <k.weber0893@gmail.com>
2018-05-23 12:17:41 -0400

    Completed anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(anagram_array)
+    match_array = []
+    anagram_array.each do |anagram|
+      if anagram.split("").sort == words.split("").sort
+        match_array << anagram
+      end
+    end
+    match_array
+  end
+
+end
